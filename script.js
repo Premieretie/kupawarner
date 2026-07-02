c0e065f26082776078052050b55d98fa3e03cc91
Premieretie <133285694+Premieretie@users.noreply.github.com>
2026-07-03 08:31:58 +1000

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const projects = [
     type: 'Commercial'
   },
   {
-    id: 'music-is-everything',
+    id: 'music-is-everything', 
     title: 'Music Is Everything - Cinematic Brand Film',
     category: 'commercial',
     image: 'commercial_music-is-everything.png',
